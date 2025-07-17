54ade415f09d0e9448fa463003ca5ea71df03b66
jamshid <jamshidalimov3464@gmail.com>
2025-07-17 19:23:27 +0500

    java misollar

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
+import java.sql.SQLOutput;
 import java.util.Locale;
+import java.util.Scanner;
 import java.util.logging.SocketHandler;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
 //String ism = "javohir";
 //        System.out.println(ism);
 //        String text = "Javohir";
@@ -13,7 +15,7 @@ public class Main {
 //        System.out.println(text.toUpperCase());
 //        System.out.println(text.toLowerCase());
 //        System.out.println(text.charAt(4));
-//        System.out.println(text.substring(1,4));
+//        System.out.println(text.substring+(1,4));
 //        String matn = "hello";
 //        for (int i = 0; i < matn.length(); i++) {
 //            System.out.println(matn.charAt(i));
@@ -24,72 +26,147 @@ public class Main {
 //        System.out.println("oxirgi yarimi "+text.substring(5,11));
 //        System.out  .println("orta yarimi "+text.substring(3,7));
 //        System.out.println(text);
+//
+//        String text = "yunusali";
+//        int length = text.length();
+//        System.out.println(text.substring(0,length/2));
+//        System.out.println(text.substring(length/2));
+//        System.out.println(text.substring(length/4,length*3/4));
+//        String text = "Salom bu split metodi haqida misol.";
+//
+//        String[] words = text.split(" ");
+//
+//        for (String word : words){
+//            System.out.println(word);
+//
+//        double a,b,c;                    1-misol;
+//        a=12;
+//        b=32;
+//        c=22;
+//        System.out.println((a+b+c)/3);
+//
+//
+//        int p,a;                           2-misol;
+//        a=12;
+//        p=4*a;
+//        System.out.println("premetri "+p);
+
+//
+//        int a,b,s,p;                       3_misol;
+//        a=11;
+//        b=22;
+//        s=a*b;
+//        p=2*(a+b);
+//        System.out.println("yuzasi "+s);
+//        System.out.println("perimetri "+p);
+//
+//
+//        int a,v,s;                        4-misol;
+//        a=11;
+//        v=a*a*a;
+//        s=6*a*a;
+//        System.out.println("hajmi "+v);
+//        System.out.println("tola sirti "+s);
 
 
-        String text = "adjngdfhdhudfdbvsjbvhdsfb";
-        System.out.println(text.length());
+//        int A = 5, B = 10, C = 15;
+//
+//        System.out.println("Dastlabki qiymatlar:");           5-misol;
+//        System.out.println("A = " + A + ", B = " + B + ", C = " + C);
+//        A = A + B + C;
+//        B = A - (B + C);
+//        C = A - (B + C);
+//        A = A - (B + C);
+//
+//        System.out.println("A = " + A + ", B = " + B + ", C = " + C);
+//
+//        Scanner scanner = new Scanner(System.in);            6-misol;
+//
+//        System.out.print("1-sonni kiriting: ");
+//        int a = scanner.nextInt();
+//        System.out.print("2-sonni kiriting: ");
+//        int a2 = scanner.nextInt();
+//
+//
+//        System.out.println("1-sonning kvadrati: " + (a * a));
+//        System.out.println("2-sonning kvadrati: " + (a2* a2));
+//
+//        System.out.print("Somda pul miqdorini kiriting ");        7-misol;
+//        double som = scanner.nextDouble();
+//        double dollar = som / 11200;
+//
+//        System.out.println(dollar);
+//        System.out.println(dollar);
+//        System.out.println(som);
+
+
+//        System.out.print("Sonni kiriting: ");            8-misol;
+//        int raqam = scanner.nextInt();
+//        System.out.println();
+//
+//
+//        int minglar =  (raqam/1000)%10;
+//        int onlar = (raqam/10)%10;
+//
+//
+//        int natija = minglar * onlar;
+//        System.out.println(minglar);
+//        System.out.println(onlar);
+//        System.out.println(natija);
+
+//
+//        System.out.println("sonni kiriting : ");      9-misol;
+//        int raqam = scanner.nextInt();
+//
+//
+//        int onlar = (raqam/10)%10;
+//        System.out.println(onlar);
+//
+//
+//        double a = 2;                     10-misol;
+//
+//        a = a * a;
+//        a = a * a;
+//        a = a * 2;
+//        a = a * 2;
+//        System.out.println("a^7 ning qiymati: " + a);
+//
+//
+//        System.out.print("Santimetrda uzunlikni kiriting: ");        11-misol;
+//        double uzunlikSantimetr = scanner.nextDouble();
+//
+//        double uzunlikMetr = uzunlikSantimetr / 100;
+//        System.out.println("Uzunlik metrda: " + uzunlikMetr);
+//
+//        System.out.println("uch xonali sonni kiriting : ");   12-misol;
+//        int son = scanner.nextInt();
+//
+//        int  yuzlarxonasi = (son/100)%10;
+//        System.out.println("yuzlar xonasi : "+yuzlarxonasi);
+
+//
+//
+//        System.out.print("Uch xonali sonni kiriting: ");   13-misol;
+//        int son = scanner.nextInt();
+//
+//
+//        int yuzlar = son / 100;
+//        int onlar = (son / 10) % 10;
+//        int birlar = son % 10;
+//
+//        int yangiSon = (onlar * 100) + (birlar * 10) + yuzlar;
+//        System.out.println(yuzlar);
+//        System.out.println(onlar);
+//        System.out.println(birlar);
+//        System.out.println(yangiSon);
+
+
+
+
 
 
     }
-
-    static boolean palindrome(String value) {
-        StringBuilder newValue = new StringBuilder();
-
-        for (int i = 0; i < value.length(); i++) {
-            newValue.append(value, value.length() - 1 - i, value.length() - i);
-        }
-
-        return value.equalsIgnoreCase(newValue.toString());
-    }
-
-    public static boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
-        return aSmile == bSmile;
-    }
-    //     return a==b ? a+b:(a+b)*2;
-
-    public static int sumDouble(int a, int b) {
-        if (a == b) {
-            return a + b;
-        } else {
-            return 2 * a + 2 * b;
-        }
-    }
-
-    public static int diff21(int n) {
-        if (n <= 21) {
-            return 21 - n;
-
-        } else {
-            return (n - 21) * 2;
-        }
-    }
-
-    public static boolean parrotTrouble(boolean talking, int hour) {
-        return talking && (hour < 7 || hour > 20);
-    }
-    public boolean makes10(int a, int b) {
-        if ((a == 10 || b == 10) && a+b==10) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    public boolean nearHundred(int n) {
-        if (Math.abs(100-n)==10 || Math.abs(200-n)==10){
-            return true;
-        } else {
-            return false;
-        }
-    }
-    public static String frontBack(String str) {
-        return "";
-    }
-    public boolean startHi(String str) {
-       return true;
-    }
-    public static String makeTags(String tag, String word) {
-       return String.format("<%s>%s</%s>", tag, word, tag);
-    }
-
 
 }
+
+
